dbc410197280b6cf0ce148de1a751dbf0098639b
Jim Stewart <jim@stewartstuff.com>
2013-06-10 19:14:53 -0500

    Initial Commit

---
+++ TestOf7/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TestOf7
+//
+//  Created by James Stewart on 6/10/13.
+//  Copyright (c) 2013 StewartStuff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JSAppDelegate class]));
+    }
+}
---
+++ TestOf7Tests/TestOf7Tests.m
@@ -0,0 +1,36 @@
+//
+//  TestOf7Tests.m
+//  TestOf7Tests
+//
+//  Created by James Stewart on 6/10/13.
+//  Copyright (c) 2013 StewartStuff. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TestOf7Tests : XCTestCase
+
+@end
+
+@implementation TestOf7Tests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
